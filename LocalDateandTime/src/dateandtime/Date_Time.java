fd25a98ee3f3f0d2e01a3b2fcc8eed87992aebe6
nayana-zone <nayanashanbhogar@gmail.com>
2024-04-23 17:07:40 +0530

    Commit

---
+++ LocalDateandTime/src/dateandtime/Date_Time.java
@@ -0,0 +1,21 @@
+package dateandtime;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class Date_Time {
+
+	public static void main(String[] args) {
+		LocalDate date = LocalDate.now();
+		System.out.println(date);
+		
+		LocalTime time = LocalTime.now();
+		System.out.println(time);
+		
+		LocalDateTime datetime = LocalDateTime.now();
+		System.out.println(datetime);
+
+	}
+
+}
